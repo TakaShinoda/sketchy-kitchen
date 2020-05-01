26c9732a2678ee2f17858c1e4a9ac4503ba3f5ec
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-02 02:23:10 +0900

    検索窓UIを実装

--- src/components/topPage/TopMain.tsx
+++ src/components/topPage/TopMain.tsx
@@ -2,6 +2,10 @@ import React, { FC } from 'react'
 import pencil from '../../assets/images/pencil.jpg'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+
 
 const useStyle = makeStyles(() => 
   createStyles({
@@ -25,7 +29,10 @@ export const TopMain: FC = () => {
     return(
         <div className={classes.background}>
             <Paper className={classes.paper}>
-                こんにちは
+                <IconButton type="submit">
+                    <SearchIcon />
+                </IconButton>
+                <InputBase　placeholder="キーワードを入力" />
             </Paper>
         </div>
     )
