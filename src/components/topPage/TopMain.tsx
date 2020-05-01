626f405a5e5eca22c879e175b61fc8d470a3a8e3
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-02 02:43:51 +0900

    useStateで入力されたキーワードを保持

--- src/components/topPage/TopMain.tsx
+++ src/components/topPage/TopMain.tsx
@@ -1,39 +1,44 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import pencil from '../../assets/images/pencil.jpg'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
+import Paper from '@material-ui/core/Paper'
+import IconButton from '@material-ui/core/IconButton'
+import SearchIcon from '@material-ui/icons/Search'
+import InputBase from '@material-ui/core/InputBase'
 
-
-const useStyle = makeStyles(() => 
+const useStyle = makeStyles(() =>
   createStyles({
     background: {
       backgroundImage: `url(${pencil})`,
       height: '100vh',
-      backgroundSize: 'cover',
+      backgroundSize: 'cover',
     },
     paper: {
-        position: 'relative',
-        marginLeft: "auto",
-        marginRight: "auto",
-        top: "10%",
-        width: "35%",
-    }
+      position: 'relative',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      top: '10%',
+      width: '35%',
+    },
   })
 )
 
 export const TopMain: FC = () => {
-    const classes = useStyle()
-    return(
-        <div className={classes.background}>
-            <Paper className={classes.paper}>
-                <IconButton type="submit">
-                    <SearchIcon />
-                </IconButton>
-                <InputBase　placeholder="キーワードを入力" />
-            </Paper>
-        </div>
-    )
+  const classes = useStyle()
+  const [keyword, setKeyword] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value)
+  }
+
+  return (
+    <div className={classes.background}>
+      <Paper className={classes.paper}>
+        <IconButton type="submit">
+          <SearchIcon />
+        </IconButton>
+        <InputBase placeholder="キーワードを入力" onChange={handleChange}/>
+      </Paper>
+    </div>
+  )
 }
