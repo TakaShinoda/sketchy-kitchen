63e2f4f45023bf3dd95120e6297bb7309e98f3c7
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-03 18:31:52 +0900

    Add: GitHubへのリンクを追加

--- src/components/topPage/TopHeader.tsx
+++ src/components/topPage/TopHeader.tsx
@@ -1,13 +1,38 @@
 import React, { FC } from 'react'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import GitHubIcon from '@material-ui/icons/GitHub'
+
+const useStyle = makeStyles(() =>
+  createStyles({
+    icon: {
+      margin: '0 0 0 auto',
+    },
+    link: {
+      textDecoration: 'none',
+      color: '#1A2459'
+    }
+  })
+)
 
 export const TopHeader: FC = () => {
+  const classes = useStyle()
+
+
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <h2>タイトル</h2>
+          <div className={classes.icon}>
+            <IconButton>
+              <a className={classes.link} href="https://github.com/TakaShinoda" target="_blank" rel="noopener noreferrer">
+                <GitHubIcon />
+              </a>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </>
