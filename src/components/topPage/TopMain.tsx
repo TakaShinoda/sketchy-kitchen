086788c656f579559a7458c7582d53770c467039
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-03 17:16:19 +0900

    Add リンク先を追加

--- src/components/topPage/TopMain.tsx
+++ src/components/topPage/TopMain.tsx
@@ -1,15 +1,48 @@
 import React, { FC } from 'react'
 import { TopHeader } from './TopHeader'
 import { useHistory } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    main: {
+      textAlign: 'center',
+    },
+    button: {
+      margin: theme.spacing(1),
+    },
+  })
+)
 
 export const TopMain: FC = () => {
+  const classes = useStyles()
   const history = useHistory()
 
   return (
     <>
       <TopHeader />
-      <h2>トップ</h2>
-      <button onClick={() => history.push('/search')}>検索</button>
+      <div className={classes.main}>
+        <h2>トップ</h2>
+        <Button
+          size="large"
+          variant="contained"
+          color="default"
+          className={classes.button}
+          onClick={() => history.push('/search')}
+        >
+          探す
+        </Button>
+
+        <Button
+          size="large"
+          variant="contained"
+          color="default"
+          className={classes.button}
+        >
+          投稿する
+        </Button>
+      </div>
     </>
   )
 }
