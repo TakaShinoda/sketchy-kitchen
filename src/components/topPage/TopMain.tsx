7c26c068d17c22348f03de8a48ae27f27f8b8585
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-03 15:23:03 +0900

    Topページを新規作成

--- src/components/topPage/TopMain.tsx
+++ src/components/topPage/TopMain.tsx
@@ -1,50 +1,15 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
+import { TopHeader } from './TopHeader'
 import { useHistory } from 'react-router-dom'
-import pencil from '../../assets/images/pencil.jpg'
-import { createStyles, makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import IconButton from '@material-ui/core/IconButton'
-import SearchIcon from '@material-ui/icons/Search'
-import InputBase from '@material-ui/core/InputBase'
-
-const useStyle = makeStyles(() =>
-  createStyles({
-    background: {
-      backgroundImage: `url(${pencil})`,
-      height: '100vh',
-      backgroundSize: 'cover',
-    },
-    paper: {
-      position: 'relative',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      top: '10%',
-      width: '35%',
-    },
-  })
-)
 
 export const TopMain: FC = () => {
-  const classes = useStyle()
-  const [keyword, setKeyword] = useState('')
   const history = useHistory()
 
-  const handleSubmit = () => {
-    history.push('/search/' + keyword)
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setKeyword(e.target.value)
-  }
-
   return (
-    <div className={classes.background}>
-      <Paper className={classes.paper} component="form" onSubmit={handleSubmit}>
-        <IconButton type="submit">
-          <SearchIcon />
-        </IconButton>
-        <InputBase placeholder="キーワードを入力" onChange={handleChange} />
-      </Paper>
-    </div>
+    <>
+      <TopHeader />
+      <h2>トップ</h2>
+      <button onClick={() => history.push('/search')}>検索</button>
+    </>
   )
 }
