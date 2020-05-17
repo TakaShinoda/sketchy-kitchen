3ec14addde1474dca3eb83c16c0328be283464d5
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-17 12:53:48 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { TopPage } from './pages/TopPage'
-import { Helmet } from 'react-helmet'
+// import { Helmet } from 'react-helmet'
 
 export const App = () => {
   return (
     <div className="App">
-      <Helmet>
-        <meta property="og:description" content="test" />
-      </Helmet>
       <TopPage />
     </div>
   )
