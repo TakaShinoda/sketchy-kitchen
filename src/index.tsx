2eb6f291b9c5ff650c955a6d42a3f2e7b1f7b8d9
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-29 23:14:49 +0900

    Delete: StrictModeを削除

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,7 @@ import { App } from './App'
 if (process.env.NODE_ENV !== 'production') {
   import('react-axe').then((axe) => {
     axe.default(React, ReactDOM, 1000)
-    ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      document.getElementById('root')
-    )
+    ReactDOM.render(<App />, document.getElementById('root'))
   })
 } else {
   ReactDOM.render(
