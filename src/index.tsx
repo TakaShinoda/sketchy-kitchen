e069152be3a94f71cc1757653b814f545eb9a5e4
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-29 23:03:31 +0900

    fix: react-axeをdynamic import

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
-import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+if (process.env.NODE_ENV !== 'production') {
+  import('react-axe').then((axe) => {
+    axe.default(React, ReactDOM, 1000)
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    )
+  })
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
