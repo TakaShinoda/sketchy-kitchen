4d00f0f3b6d57efb616b171e0a2f3057c41be99f
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-30 14:35:05 +0900

    fix: mainタグを追記

--- src/pages/AllRecipesPage.tsx
+++ src/pages/AllRecipesPage.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react'
 import { TopHeader } from '../components/topPage/TopHeader'
-import { AllRecipesList } from '../components/allRecipePage/AllRecipesList';
-
+import { AllRecipesList } from '../components/allRecipePage/AllRecipesList'
 
 export const AllRecipesPage: FC = () => {
   return (
     <>
       <TopHeader />
-      <AllRecipesList />
+      <main>
+        <AllRecipesList />
+      </main>
     </>
   )
 }
